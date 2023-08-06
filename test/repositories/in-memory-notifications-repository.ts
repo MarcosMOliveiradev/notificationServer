a9fad90bc4f02d7e2c64567900ca54e4c73d6830
Marcos Monteiro <marcos.moliveira@outlook.com>
2023-08-06 18:44:08 -0300

    cancel notification

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -9,4 +9,25 @@ export class InMemoryNotificationsRepository
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+    return notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIdex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIdex >= 0) {
+      this.notifications[notificationIdex] = notification;
+    }
+  }
 }
